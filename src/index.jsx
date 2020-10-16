01b4c3dce301f9b4553d110477bbca879f7590fa
Meinte Boersma <meinte.boersma@gmail.com>
2020-10-16 13:57:29 +0200

    initial code base to get the session going

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { render } from "react-dom"
+
+require("./styling.css")
+
+
+const App = () => <span>do stuff!</span>
+
+
+render(
+    <App />,
+    document.getElementById("root")
+)
+
